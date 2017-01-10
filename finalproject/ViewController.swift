52fef5314e7f456f5705471f1ae0cba4261fb122
Aynel <aynell__@hotmail.com>
2017-01-10 13:32:08 +0100

    Initial Commit

---
+++ finalproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  finalproject
+//
+//  Created by Aynel Gül on 10-01-17.
+//  Copyright © 2017 Aynel Gül. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
