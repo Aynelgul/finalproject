0b8e7d732fabe4fd541fb0aa5ddf21997587cbe6
Aynel <aynell__@hotmail.com>
2017-01-24 15:44:52 +0100

    tableview error/crash opgelost

--- finalproject/ShowTipsAndBuddiesViewController.swift
+++ finalproject/ShowTipsAndBuddiesViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ShowTipsAndBuddiesViewController: UIViewController {
+class ShowTipsAndBuddiesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: Variables
+    var items = ["test1", "test2", "test3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tipCell", for: indexPath) as! TipsBuddiesCell
+        
+        let item = items[indexPath.row]
+        cell.titleLabel.text = item
+       
+        return cell
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
